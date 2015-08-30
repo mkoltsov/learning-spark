14193a4af01125b6dec47355401c31a47841f48a
mkoltsov <root@javabean.ru>
2015-08-30 12:03:01 +0200

    execution script added

---
+++ scripts/keyPerformanceConsiderations.scala
@@ -0,0 +1,16 @@
+# Wildcard input that may match thousands of files
+>>> input = sc.textFile("s3n://log-files/2014/*.log")
+>>> input.getNumPartitions()
+35154
+# A filter that excludes almost all data
+>>> lines = input.filter(lambda line: line.startswith("2014-10-17"))
+>>> lines.getNumPartitions()
+35154
+# We coalesce the lines RDD before caching
+>>> lines = lines.coalesce(5).cache()
+>>> lines.getNumPartitions()
+5
+# Subsequent analysis can operate on the coalesced RDD...
+>>> lines.count()
+
+// use repartition() in case you need to reshuffle & redistribute the data
