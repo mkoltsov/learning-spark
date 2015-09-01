dbc2c7990f0f685daa7e978b70dd0d5192a452a7
mkoltsov <root@javabean.ru>
2015-09-01 11:27:17 +0200

    execution script added

--- scripts/execution.scala
+++ scripts/execution.scala
@@ -3,3 +3,6 @@ val input = sc.textFile("input.txt")
 val processed = input.filter(x => x.length>0).map(x=>x.split(" "))
 
 val counts = processed.map(words=> (words(0), 1)).reduceByKey{(a,b) => a+b}
+
+println(input.toDebugString)
+println(counts.toDebugString)
