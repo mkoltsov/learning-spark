7a8f16983cdc744ea43d2ee390759cc75f360a56
mkoltsov <root@javabean.ru>
2015-09-09 00:00:25 +0200

    parameters to run on a cluster example added

--- scripts/parameters.scala
+++ scripts/parameters.scala
@@ -1,1 +1,22 @@
-#
+//## Submitting a Java application to Standalone cluster mode
+$ ./bin/spark-submit \
+  --master spark://hostname:7077 \
+  --deploy-mode cluster \
+  --class com.databricks.examples.SparkExample \
+  --name "Example Program" \
+  --jars dep1.jar,dep2.jar,dep3.jar \
+  --total-executor-cores 300 \
+  --executor-memory 10g \
+  myApp.jar "options" "to your application" "go here"
+
+  //# Submitting a Python application in YARN client mode
+$ export HADOP_CONF_DIR=/opt/hadoop/conf
+$ ./bin/spark-submit \
+  --master yarn \
+  --py-files somelib-1.2.egg,otherlib-4.4.zip,other-file.py \
+  --deploy-mode client \
+  --name "Example Program" \
+  --queue exampleQueue \
+  --num-executors 40 \
+  --executor-memory 10g \
+  my_script.py "options" "to your application" "go here"
