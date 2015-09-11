2c9bec196926a2f946ea0b17faa02489ae63dea1
mkoltsov <root@javabean.ru>
2015-09-11 22:04:55 +0200

    example of piping an R program to Spark added

---
+++ scripts/pipingWithRProgram.scala
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+library("Imap")
+f <- file("stdin")
+open(f)
+while(length(line <- readLines(f,n=1)) > 0) {
+  # process line
+  contents <- Map(as.numeric, strsplit(line, ","))
+  mydist <- gdist(contents[[1]][1], contents[[1]][2],
+                  contents[[1]][3], contents[[1]][4],
+                  units="m", a=6378137.0, b=6356752.3142, verbose = FALSE)
+  write(mydist, stdout())
+}
+
+
+// Compute the distance of each call using an external R program
+// adds our script to a list of files for each node to download with this job
+val distScript = "./src/R/finddistance.R"
+val distScriptName = "finddistance.R"
+sc.addFile(distScript)
+val distances = contactsContactLists.values.flatMap(x => x.map(y =>
+  s"${y.contactlay},${y.contactlong},${y.mylat},${y.mylong}")).pipe(Seq(
+    SparkFiles.get(distScriptName)))
+println(distances.collect().toList)
