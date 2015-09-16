32d3a374a21c8953203d20b2df66e5dc100d348b
mkoltsov <root@javabean.ru>
2015-09-16 21:12:37 +0200

    example of spark accumulator added

---
+++ scripts/accumulator.scala
@@ -0,0 +1,14 @@
+val file = sc.textFile("../CHANGES.txt")
+
+val blankLines = sc.accumulator(0)
+
+val callSigns = file.flatMap(
+	line => {
+			if (line==""){
+				blankLines+=1
+			}
+		line.split(" ")
+	})
+
+callSigns.saveAsTextFile("output.txt")
+println(s"Blank lines: $blankLines.value")
