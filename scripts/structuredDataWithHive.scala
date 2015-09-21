7b848be8d2c7e427b1317d5cb7fc9a16e6823cdb
mkoltsov <root@javabean.ru>
2015-09-21 19:10:33 +0200

    example of interconnection with Apache Hive added

--- scripts/structuredDataWithHive.scala
+++ scripts/structuredDataWithHive.scala
@@ -5,9 +5,10 @@ val hiveCtx = new HiveContext(sc);
 val tweets = hiveCtx.jsonFile("tweets.json")
 tweets.registerTempTable("tweets")
 val results = hiveCtx.sql("SELECT user.name, text FROM tweets")
+println(results.first().getString(0))
 
 //SQL
-val hiveCtx = new HiveContext(sc)
-val rows = hiveCtx.sql("SELECT name, age FROM users")
-val firstRow = rows.first()
-println(firstRow.getString(0)) // Field 0 is the name
+// val hiveCtx = new HiveContext(sc)
+// val rows = hiveCtx.sql("SELECT name, age FROM users")
+// val firstRow = rows.first()
+// println(firstRow.getString(0)) // Field 0 is the name
