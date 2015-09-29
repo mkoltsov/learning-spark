6b65b4f599c0e279ad0785093e86e252462a0f31
mkoltsov <root@javabean.ru>
2015-09-29 15:57:21 +0200

    an example of partitioning added

--- scripts/partitioning.scala
+++ scripts/partitioning.scala
@@ -1,3 +1,4 @@
+// -------------
 val pairs = sc.parallelize(List((1,1), (2,2), (3,3)))
 
 println(pairs.partitioner)
@@ -6,7 +7,7 @@ val partitioned = pairs.partitionBy(new spark.HashPartitioner(2))
 
 println(partitioned.partitioner)
 
-//INEFFICIENT APPROACH
+//----------------INEFFICIENT APPROACH
 
 // Initialization code; we load the user info from a Hadoop SequenceFile on HDFS.
 // This distributes elements of userData by the HDFS block where they are found,
@@ -33,3 +34,45 @@ def processNewLogs(logFileName: String) {
   println("Number of visits to non-subscribed topics: " + offTopicVisits)
 }
 
+//--------------- Scala PageRank
+// Assume that our neighbor list was saved as a Spark objectFile
+val links = sc.objectFile[(String, Seq[String])]("links")
+              .partitionBy(new HashPartitioner(100))
+              .persist()
+
+// Initialize each page's rank to 1.0; since we use mapValues, the resulting RDD
+// will have the same partitioner as links
+var ranks = links.mapValues(v => 1.0)
+
+// Run 10 iterations of PageRank
+for (i <- 0 until 10) {
+  val contributions = links.join(ranks).flatMap {
+    case (pageId, (links, rank)) =>
+      links.map(dest => (dest, rank / links.size))
+  }
+  ranks = contributions.reduceByKey((x, y) => x + y).mapValues(v => 0.15 + 0.85*v)
+}
+
+// Write out the final ranks
+ranks.saveAsTextFile("ranks")
+
+// -----------Scala custom partitioner
+class DomainNamePartitioner(numParts: Int) extends Partitioner {
+  override def numPartitions: Int = numParts
+  override def getPartition(key: Any): Int = {
+    val domain = new Java.net.URL(key.toString).getHost()
+    val code = (domain.hashCode % numPartitions)
+    if (code < 0) {
+      code + numPartitions  // Make it non-negative
+    } else {
+      code
+    }
+  }
+  // Java equals method to let Spark compare our Partitioner objects
+  override def equals(other: Any): Boolean = other match {
+    case dnp: DomainNamePartitioner =>
+      dnp.numPartitions == numPartitions
+    case _ =>
+      false
+  }
+}
