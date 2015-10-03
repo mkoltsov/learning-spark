5379b2c1baf529b197eb5f7e018dda04eb62ab89
mkoltsov <root@javabean.ru>
2015-10-03 16:13:36 +0200

    key value example added

--- scripts/keyValue.scala
+++ scripts/keyValue.scala
@@ -60,3 +60,9 @@ result.collectAsMap().map(println(_))
 val data = Seq(("a", 3), ("b", 4), ("a", 1))
 sc.parallelize(data).reduceByKey((x, y) => x + y)    // Default parallelism
 sc.parallelize(data).reduceByKey((x, y) => x + y, 10)    // Custom parallelism
+
+
+val repart = rdd1.repartition(20)
+println(repart.partitions.size)
+val coalesced = rdd1.coalesce(20, true)
+println(coalesced.partitions.size)
