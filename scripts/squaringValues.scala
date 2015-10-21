782fe54d00edaecafeb1b74d0eba4f367b3bd170
mkoltsov <root@javabean.ru>
2015-10-21 23:32:25 +0200

    flatMap example added

--- scripts/squaringValues.scala
+++ scripts/squaringValues.scala
@@ -5,3 +5,9 @@ val result = input.map(x => x * x)
 val result2 = input2.map(x => x * x)
 println(result.collect().mkString(","))
 println(result2.collect().mkString(","))
+
+val someInput = sc.parallelize(List("some words", "hello", " world"))
+
+val flat = someInput.flatMap(x => x.split(" "))
+
+println(flat.first())
