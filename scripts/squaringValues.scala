b4849ee8b536cb25987204dcfbc7e632fdaf95c7
mkoltsov <root@javabean.ru>
2015-10-23 23:10:43 +0200

    scala generic script added

---
+++ scripts/squaringValues.scala
@@ -0,0 +1,7 @@
+val input = sc.parallelize(Array(1,2,3,4,5,6))
+val input2 = sc.parallelize(List(1,2,3,4,5,6))
+
+val result = input.map(x => x * x)
+val result2 = input2.map(x => x * x)
+println(result.collect().mkString(","))
+println(result2.collect().mkString(","))
