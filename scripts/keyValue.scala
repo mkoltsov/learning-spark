6badd514ec0fc0792b88bada8a6a9f1a7ed071c3
mkoltsov <root@javabean.ru>
2015-10-29 23:43:24 +0100

    working with key value pairs example added

--- scripts/keyValue.scala
+++ scripts/keyValue.scala
@@ -3,3 +3,5 @@ val lines = sc.parallelize(List("ch ef", "pu pa", "pa pa"))
 val pairs = lines.map(x => (x.split(" ")(0), x))
 
 println(pairs.collect().mkString(","))
+sc.parallelize(List(("chef", "pupa"), ("papa", "bupa"))).collect().foreach(println)
+
