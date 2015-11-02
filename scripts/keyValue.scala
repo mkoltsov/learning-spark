638720ed348780e2609111375bd4873b0ac021f3
mkoltsov <root@javabean.ru>
2015-11-02 14:28:59 +0100

    key value example added

--- scripts/keyValue.scala
+++ scripts/keyValue.scala
@@ -5,3 +5,7 @@ val pairs = lines.map(x => (x.split(" ")(0), x))
 println(pairs.collect().mkString(","))
 sc.parallelize(List(("chef", "pupa"), ("papa", "bupa"))).collect().foreach(println)
 
+val example = sc.parallelize(List((1,2), (3,4),(3,6)))
+
+println(example.reduceByKey((x,y) => x+y).foreach(println))
+
