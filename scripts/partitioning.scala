92396f6fa3b2f01222fcf39ad60480bf6a5f6157
mkoltsov <root@javabean.ru>
2015-11-04 15:39:27 +0100

    an example of partitioning added

---
+++ scripts/partitioning.scala
@@ -0,0 +1,20 @@
+//INEFFICIENT APPROACH
+
+// Initialization code; we load the user info from a Hadoop SequenceFile on HDFS.
+// This distributes elements of userData by the HDFS block where they are found,
+// and doesn't provide Spark with any way of knowing in which partition a
+// particular UserID is located.
+val sc = new SparkContext(...)
+val userData = sc.sequenceFile[UserID, UserInfo]("hdfs://...").persist()
+
+// Function called periodically to process a logfile of events in the past 5 minutes;
+// we assume that this is a SequenceFile containing (UserID, LinkInfo) pairs.
+def processNewLogs(logFileName: String) {
+  val events = sc.sequenceFile[UserID, LinkInfo](logFileName)
+  val joined = userData.join(events)// RDD of (UserID, (UserInfo, LinkInfo)) pairs
+  val offTopicVisits = joined.filter {
+    case (userId, (userInfo, linkInfo)) => // Expand the tuple into its components
+      !userInfo.topics.contains(linkInfo.topic)
+  }.count()
+  println("Number of visits to non-subscribed topics: " + offTopicVisits)
+}
