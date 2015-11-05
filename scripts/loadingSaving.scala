2747f46e03d8812d201aee634e9b4ed2c8b1ca11
mkoltsov <root@javabean.ru>
2015-11-05 01:04:01 +0100

    loading data examples added

--- scripts/loadingSaving.scala
+++ scripts/loadingSaving.scala
@@ -1,3 +1,7 @@
 val input = sc.wholeTextFiles("../")
 val a = input.collectAsMap()
 println(a.keys.reduce((x,y)=>x+";"+y))
+
+val result = input.mapValues{y => 
+val nums = y.split(" ").map(x=>x.toDouble)
+nums.sum/nums.size.toDouble}
