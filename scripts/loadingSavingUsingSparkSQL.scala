52b753f250e320e818682257be68e245881e638e
mkoltsov <root@javabean.ru>
2015-11-12 20:01:25 +0100

    an example of saving&loading added

---
+++ scripts/loadingSavingUsingSparkSQL.scala
@@ -0,0 +1,5 @@
+import org.apache.spark.sql.hive.HiveContext
+
+val hiveCtx = new HiveContext(sc)
+val rows = hiveCtx.sql("SELECT key, value FROM mytable")
+val keys = rows.map(row => row.getInt(0))
