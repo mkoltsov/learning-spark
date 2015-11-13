3a39d01293cde04706fce8da433325d87867c406
mkoltsov <root@javabean.ru>
2015-11-13 18:03:18 +0100

    final commit

--- scripts/loadingSavingUsingSparkSQL.scala
+++ scripts/loadingSavingUsingSparkSQL.scala
@@ -3,3 +3,5 @@ import org.apache.spark.sql.hive.HiveContext
 val hiveCtx = new HiveContext(sc)
 val rows = hiveCtx.sql("SELECT key, value FROM mytable")
 val keys = rows.map(row => row.getInt(0))
+
+rows.saveAsTable("tblName")
